75be81fde6d9c5597524569490286bdd9a32b1f8
rickydevjs <75860927+rickydevjs@users.noreply.github.com>
2021-01-06 17:14:42 +0100

    Add files via upload
    petrucciii_bot

---
+++ telegrambot.py
@@ -0,0 +1,70 @@
+#by ricky
+import telebot
+
+API_TOKEN = '1571622489:AAH4dHTNmKC9-c_jK2cFvRwPP0fsiWtu_QQ'
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+i am a chat bot created by @petruciii\
+""")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    if message.text == 'hi' :
+        bot.reply_to(message, 'hey, how are you?')
+    elif message.text == 'i am fine, thanks, and you?':
+        bot.reply_to(message, 'i am fine, too')
+    elif message.text == 'i am feeling bad' :
+        bot.reply_to(message, 'oh, i am so sorry')
+    elif message.text == 'i am feeling bad, and you?':
+        bot.reply_to(message, 'oh, i am so sorry. i am fine, thanks')
+    elif message.text == 'fuck you':
+        bot.reply_to(message, 'shut up! son of a bitch!')
+    elif message.text == 'i love you':
+        bot.reply_to(message, 'let us remain friends')
+    elif message.text == 'my name is dado':
+        bot.reply_to(message, 'HELLO RUBY')
+    elif message.text == 'who is your creator?':
+        bot.reply_to(message, '@petrucciii')
+    elif message.text == 'who is petrucciii?':
+        bot.reply_to(message, 'he is the programmer who made me')
+    elif message.text == 'hello':
+        bot.reply_to(message, 'hey, how are you?')
+    elif message.text == 'hey':
+        bot.reply_to(message, 'hey, how are you')
+    elif message.text == 'i am fine, thanks':
+        bot.reply_to(message, 'i am fine, too')
+    elif message.text == 'i am fine':
+        bot.reply_to(message, 'i am fine, too')
+    elif message.text == 'i am fine, and you?':
+        bot.reply_to(message, 'i am fine, too')
+    elif message.text == 'are you stupid?':
+        bot.reply_to(message, 'shut up! son of a bitch!')
+    elif message.text == 'you are stupid':
+        bot.reply_to(message, 'shut up! son of a bitch!')
+    elif message.text == 'bitch':
+        bot.reply_to(message, 'shut up! son of a bitch!')
+    elif message.text == 'how old are you?':
+        bot.reply_to(message, 'i have 1 day')
+    elif message.text == 'who is your dad?':
+        bot.reply_to(message, 'i have not a dad. i have a creator: @petrucciii')
+    elif message.text == 'who is your mum?':
+        bot.reply_to(message, 'i have not a mum. i have a creator: @petrucciii')
+    elif message.text == 'who are your parents?':
+        bot.reply_to(message, 'i have not parents. i have a creator: @petrucciii')
+    elif message.text == 'what is your favourite color?:
+        bot.reply_to(message, 'my favourite color is blue')
+    elif message.text == 'what is your favourite food?:
+        bot.reply_to(message, 'i like so much the pizza!')
+    else :
+        bot.reply_to(message, 'i am sorry, i did not understand! i am still learning!')
+
+
+
+bot.polling()
